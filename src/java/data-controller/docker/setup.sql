89f315e0981f6f2408f0e54918498a70cd5037e2
Tristan <tristan.morisot@viacesi.fr>
2019-06-24 10:27:59 +0200

    :bug: Change sql setup cmd :bug:

--- src/java/data-controller/docker/setup.sql
+++ src/java/data-controller/docker/setup.sql
@@ -1,31 +1,47 @@
-create table Metrics
-(
-    id    int auto_increment
-        primary key,
-    value int not null
-);
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
 
-create table Users
-(
-    id      int auto_increment
-        primary key,
-    name    varchar(50) not null,
-    surname varchar(50) not null,
-    email   varchar(50) not null
-);
 
-create table Devices
-(
-    id          int auto_increment
-        primary key,
-    model       varchar(50) not null,
-    mac_address varchar(50) not null,
-    date        date        not null,
-    id_Metrics  int         not null,
-    id_Users    int         not null,
-    constraint Devices_Metrics_FK
-        foreign key (id_Metrics) references Metrics (id),
-    constraint Devices_Users0_FK
-        foreign key (id_Users) references Users (id)
-);
+#------------------------------------------------------------
+# Table: Users
+#------------------------------------------------------------
+
+CREATE TABLE Users(
+        id      Int  Auto_increment  NOT NULL ,
+        name    Varchar (50) NOT NULL ,
+        surname Varchar (50) NOT NULL ,
+        email   Varchar (50) NOT NULL
+	,CONSTRAINT Users_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Devices
+#------------------------------------------------------------
+
+CREATE TABLE Devices(
+        id          Int  Auto_increment  NOT NULL ,
+        model       Varchar (50) NOT NULL ,
+        mac_address Varchar (50) NOT NULL ,
+        id_Users    Int
+	,CONSTRAINT Devices_PK PRIMARY KEY (id)
+
+	,CONSTRAINT Devices_Users_FK FOREIGN KEY (id_Users) REFERENCES Users(id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Metrics
+#------------------------------------------------------------
+
+CREATE TABLE Metrics(
+        id         Int  Auto_increment  NOT NULL ,
+        value      Int NOT NULL ,
+        date       Date NOT NULL ,
+        id_Devices Int NOT NULL
+	,CONSTRAINT Metrics_PK PRIMARY KEY (id)
+
+	,CONSTRAINT Metrics_Devices_FK FOREIGN KEY (id_Devices) REFERENCES Devices(id)
+)ENGINE=InnoDB;
 
