29517064e6d59d287c047d75e36895fa07fd7dd1
Tristan <tristan.morisot@viacesi.fr>
2019-06-30 19:01:40 +0200

    update test logic

--- src/java/data-controller/src/test/java/data/QueryRouterTest.java
+++ src/java/data-controller/src/test/java/data/QueryRouterTest.java
@@ -1,17 +1,25 @@
 package data;
 
+import Model.MessageModel;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class QueryRouterTest {
 
-    QueryRouter router = new QueryRouter();
+    QueryRouter router;
+
+    {
+        try {
+            router = new QueryRouter();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     void simpleExec() {
-        String res = router.dispatch("GetDevices");
+        String res = router.dispatch(new MessageModel("connectorid", "target", "aabb", "GetDevices", null));
         System.out.println(res);
     }
-
 }
